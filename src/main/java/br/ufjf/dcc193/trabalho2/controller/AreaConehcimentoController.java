3fdb2a2c0f1a1ed3ac4e679ddc39fa14d7f3e7b2
PedroFF <soufreitas.pedro@gmail.com>
2019-05-23 14:26:51 -0300

    Criando controller da área de conhecimento

--- src/main/java/br/ufjf/dcc193/trabalho2/controller/AreaConehcimentoController.java
+++ src/main/java/br/ufjf/dcc193/trabalho2/controller/AreaConehcimentoController.java
@@ -1,7 +1,75 @@
 package br.ufjf.dcc193.trabalho2.controller;
 
+import br.ufjf.dcc193.trabalho2.model.AreaConhecimento;
+import br.ufjf.dcc193.trabalho2.repository.AreaConhecimentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class AreaConehcimentoController {
+
+    @Autowired
+    private AreaConhecimentoRepository rep;
+
+    @GetMapping("areas.html")
+    public ModelAndView areasConhecimento() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("areasConhecimento");
+        List<AreaConhecimento> areasConhecimento = rep.findAll();
+        mv.addObject("areasConhecimento", areasConhecimento);
+        return mv;
+    }
+
+    @GetMapping("areas/{id}.html")
+    public ModelAndView areaConhecimento(@PathVariable Long id)
+    {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("area");
+        Optional<AreaConhecimento> areasConhecimento = rep.findById(id);
+        mv.addObject("areasConhecimento",areasConhecimento);
+        return mv;
+    }
+
+    @GetMapping("areas-cadastrar.html")
+    public ModelAndView areaCadastrar()
+    {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("area-cadastrar");
+        return mv;
+    }
+
+    @PostMapping("areas-cadastrar.html")
+    public RedirectView areaCadastrarPost(AreaConhecimento areaConhecimento){
+        rep.save(areaConhecimento);
+        return new RedirectView("/areas.html?cadastrado=true");
+    }
+
+    @GetMapping("/areas-editar/{id}.html")
+    public ModelAndView areaAlterar(@PathVariable Long id){
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("area",rep.getOne(id));
+        mv.setViewName("area-editar");
+        return mv;
+    }
+    @PostMapping("/areas-editar.html")
+    public RedirectView areaAlterarPost(AreaConhecimento area){
+        rep.save(area);
+        return new RedirectView("/areas.html?alterado=true");
+    }
+
+    @GetMapping("/areas-deletar/{id}.html")
+    public RedirectView areaDeletar(@PathVariable Long id){
+        rep.deleteById(id);
+        return new RedirectView("/areas.html?deletado=true");
+    }
+
+
 }
