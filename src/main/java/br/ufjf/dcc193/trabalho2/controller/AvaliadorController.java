162dcdccc4429f359e568d4da080395b222926d0
Rian Alves <rianalves2009.ra@gmail.com>
2019-06-14 13:04:51 -0300

    Alteração no controller do Avaliador

--- src/main/java/br/ufjf/dcc193/trabalho2/controller/AvaliadorController.java
+++ src/main/java/br/ufjf/dcc193/trabalho2/controller/AvaliadorController.java
@@ -2,11 +2,17 @@ package br.ufjf.dcc193.trabalho2.controller;
 
 import br.ufjf.dcc193.trabalho2.model.Avaliador;
 import br.ufjf.dcc193.trabalho2.repository.AvaliadorRepository;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -30,18 +36,29 @@ public class AvaliadorController {
         return mv;
     }
 
-    @GetMapping("avaliadores-cadastrar.html")
+    @RequestMapping(value = "/novo.html", method = RequestMethod.GET)
     public ModelAndView avaliadorCadastrar()
     {
+        Avaliador avaliador = new Avaliador();
         ModelAndView mv = new ModelAndView();
         mv.setViewName("avaliadores-cadastrar");
+        mv.addObject("avaliador", avaliador);
         return mv;
     }
 
-    @PostMapping("avaliadores-cadastrar.html")
-    public RedirectView avaliadorCadastrar(Avaliador avaliador){
+    @PostMapping(value = "/criar.html")
+    public ModelAndView avaliadorSalvar(@Valid Avaliador avaliador, BindingResult binding){
+    
+        ModelAndView mv = new ModelAndView();
+        if (binding.hasErrors()) {
+            mv.setViewName("avaliadores-cadastrar");
+            mv.addObject("avaliador", avaliador);
+            return mv;
+        }
         rep.save(avaliador);
-        return new RedirectView("/avaliadores.html?cadastrado=true");
+        mv.setViewName("redirect:loin.html");
+       return mv;
+       
     }
 
 
